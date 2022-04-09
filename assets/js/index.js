fe28962d45fc2fade506f926b31dd4fd8e2ffe4d
Yese <yesenia@uber.com>
2022-04-08 19:42:55 -0500

    Landing page

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+if (document.querySelector('#modal_video_uber')) {
+    const iconClose = document.querySelectorAll('.icon-close');
+    const btnClose = document.querySelectorAll('.btn-close');
+
+    const closeModal = () => {
+        document.querySelector('#video_uber').pause();
+        document.querySelector('#video_registro_app').pause();
+        document.querySelector('#video_iniciar_sesion').pause();    
+    }
+
+    iconClose.forEach(element => element.addEventListener('click', closeModal))
+    btnClose.forEach(element => element.addEventListener('click', closeModal))
+}
