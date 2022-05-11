519649650b53125d3128511dfa689d3df70ed2b2
Yesenia71 <yese3982@gmail.com>
2022-05-11 18:07:06 -0500

    videos, modales y JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,11 @@
-if (document.querySelector('#modal_video_uber')) {
-    const iconClose = document.querySelectorAll('.icon-close');
-    const btnClose = document.querySelectorAll('.btn-close');
+const iconClose = document.querySelectorAll('.icon-close');
+const btnClose = document.querySelectorAll('.btn-close');
 
-    const closeModal = () => {
-        document.querySelector('#video_uber').pause();
-        document.querySelector('#video_registro_app').pause();
-        document.querySelector('#video_iniciar_sesion').pause();    
-    }
-
-    iconClose.forEach(element => element.addEventListener('click', closeModal))
-    btnClose.forEach(element => element.addEventListener('click', closeModal))
+const closeModal = () => {
+    document.querySelector('#video_uber').pause();
+    document.querySelector('#video_registro_app').pause();
+    document.querySelector('#video_iniciar_sesion').pause();
 }
+
+iconClose.forEach(element => element.addEventListener('click', closeModal))
+btnClose.forEach(element => element.addEventListener('click', closeModal))
