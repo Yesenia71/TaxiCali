0c3f6d91af2aac4d7b9a179207b241833fd4a459
Yesenia71 <yese3982@gmail.com>
2022-06-14 12:01:41 -0500

    videos

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,8 @@ const closeModal = () => {
     document.querySelector('#video_uber').pause();
     document.querySelector('#video_registro_app').pause();
     document.querySelector('#video_iniciar_sesion').pause();
+    document.querySelector('#video_iniciar_viaje').pause();
+    document.querySelector('#video_conectar_app').pause();
 }
 
 iconClose.forEach(element => element.addEventListener('click', closeModal))
